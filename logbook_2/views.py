04a549ebe1bf1d4f750551e82f361942f81e3e2a
Dheeraj Agrawal <dheeraja486@gmail.com>
2017-08-21 15:15:10 +0530

    Add meeting details

--- logbook_2/views.py
+++ logbook_2/views.py
@@ -1,5 +1,7 @@
-from django.http import HttpResponse,request
+from django.http import HttpResponse,request,HttpResponseRedirect
 from django.shortcuts import render
+from django.utils import timezone
+
 from .models import MeetingDetails
 from .models import StudDetails
 from .models import Students
@@ -10,12 +12,42 @@ def index(request):
     return render(request, 'logbook_2/index.html')
 
 def new_entry(request):
+    if(request.method == "POST"):
+        dt= request.POST['dt']
+        st = request.POST['st']
+        et = request.POST['et']
+        objjective = request.POST['obj']
+        entry = MeetingDetails(date=dt,start_time=st,end_time=et,today_objective=objjective)
+        entry.save()
+        st = Students.objects.all()
+        for s in st:
+            idq = str(s.id)
+            if(request.POST.get(idq,"Not present") == "on"):
+                sq = "ob"+idq
+                ob = request.POST[sq]
+                student=StudDetails(name=s,objective=ob,present=True,date=dt)
+                student.save()
 
+        return HttpResponseRedirect("/logbook/")
+    else:
+        students = Students.objects.all()
+        return render(request,'logbook_2/add.html',{'students':students})
 
 
 def past_entry(request):
     data = MeetingDetails.objects.order_by('-date')
     students = StudDetails.objects.all()
     return render(request,'logbook_2/entries.html',{'data':data,'students':students})
+
+def add_student(request):
+    if request.method == "POST":
+        name = request.POST['name']
+        id = request.POST['id']
+        student = Students(name=name,id=id)
+        student.save()
+        return HttpResponseRedirect('/logbook/')
+
+    else:
+        return render(request,'logbook_2/add_student.html')
 
 
